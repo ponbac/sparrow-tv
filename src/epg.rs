ec0a27ae52fe10e153e276afc99124aebf1abf4b
ponbac <root@ponbac.xyz>
2024-10-19 08:41:16 +0200

    parse the xml

--- src/epg.rs
+++ src/epg.rs
@@ -3,30 +3,81 @@ use std::fs::File;
 use std::io::BufReader;
 
 #[derive(Debug, Deserialize)]
-pub struct EPG {
-    pub channels: Vec<Channel>,
+struct Epg {
+    #[serde(rename = "channel", default)]
+    channels: Vec<Channel>,
+    #[serde(rename = "programme", default)]
+    programmes: Vec<Programme>,
 }
 
 #[derive(Debug, Deserialize)]
-pub struct Channel {
+struct Channel {
     #[serde(rename = "id")]
-    pub id: String,
-    pub name: String,
-    pub schedule: Vec<Program>,
+    id: String,
+    #[serde(rename = "display-name")]
+    display_name: String,
+    #[serde(default)]
+    icon: Option<Icon>,
 }
 
 #[derive(Debug, Deserialize)]
-pub struct Program {
-    pub title: String,
-    pub start: String, // You might want to use a DateTime type for better handling
-    pub end: String,   // You might want to use a DateTime type for better handling
+struct Icon {
+    #[serde(rename = "src")]
+    src: String,
 }
 
-impl EPG {
-    pub fn from_xml(file_path: &str) -> Result<Self, Box<dyn std::error::Error>> {
-        let file = File::open(file_path)?;
-        let reader = BufReader::new(file);
-        let epg = serde_xml_rs::from_reader(reader)?;
-        Ok(epg)
+#[derive(Debug, Deserialize)]
+struct Programme {
+    #[serde(rename = "start")]
+    start: String,
+    #[serde(rename = "stop")]
+    stop: String,
+    #[serde(rename = "channel")]
+    channel: String,
+    title: String,
+    desc: String,
+}
+
+pub fn parse_epg() -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open("examples/epg.xml")?;
+    let reader = BufReader::new(file);
+
+    let epg: Epg = serde_xml_rs::from_reader(reader)?;
+
+    println!("Channels:");
+    for channel in &epg.channels {
+        println!("  ID: {}", channel.id);
+        println!("  Name: {}", channel.display_name);
+        println!(
+            "  Icon: {}",
+            channel
+                .icon
+                .as_ref()
+                .map(|icon| &icon.src)
+                .unwrap_or(&String::new())
+        );
+        println!();
+    }
+
+    println!("Programmes:");
+    for programme in &epg.programmes {
+        println!("  Channel: {}", programme.channel);
+        println!("  Start: {}", programme.start);
+        println!("  Stop: {}", programme.stop);
+        println!("  Title: {}", programme.title);
+        println!("  Description: {}", programme.desc);
+        println!();
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_epg() -> Result<(), Box<dyn std::error::Error>> {
+        parse_epg()
     }
 }
